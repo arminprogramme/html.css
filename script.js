1ae788f37f065212455398aaf7594e2bac5639b0
arminprogramme <ali.faraji42@yahoo.com>
2021-11-01 22:08:53 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+< script type = "text/javascript"src = "vanilla-tilt.js" > 
+</script>  <script type = "text/javascript" >
+    VanillaTilt.init(document.querySelector(".container"), {
+        max: 25,
+        speed: 400,
+        glare: true,
+        "max-glare": 0.2,
+    });
